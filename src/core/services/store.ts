8f5cb867a96ccfe1e47f11b61db60afa2a7c322b
Çağdaş Aldoğan <cagdas.aldogan00@gmail.com>
2023-04-26 11:55:27 +0300

    unused services removed

--- src/core/services/store.ts
+++ src/core/services/store.ts
@@ -1,39 +1,25 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import checkTokenReducer from './app/checkToken/checkToken.slice';
 import setApiErrorReducer from './app/setApiError/setApiError.slice';
 import setAppMountedReducer from './app/setAppMounted/setAppMounted.slice';
 import setMeReducer from './app/setMe/setMe.slice';
 import setPathnameReducer from './app/setPathname/setPathname.slice';
-import loginReducer from './auth/login/login.slice';
 import setRemoteConfigReducer from './firebase/setRemoteConfig/setRemoteConfig.slice';
-import getMeReducer from './user/getMe/getMe.slice';
 
 const appReducer = combineReducers({
-  checkToken: checkTokenReducer,
   setApiError: setApiErrorReducer,
   setAppMounted: setAppMountedReducer,
   setMe: setMeReducer,
   setPathname: setPathnameReducer,
 });
 
-const authReducer = combineReducers({
-  login: loginReducer,
-});
-
 const firebaseReducer = combineReducers({
   setRemoteConfig: setRemoteConfigReducer,
-});
-
-const userReducer = combineReducers({
-  getMe: getMeReducer,
 });
 
 const store = configureStore({
   reducer: {
     app: appReducer,
-    auth: authReducer,
     firebase: firebaseReducer,
-    user: userReducer,
   },
 });
 
---
+++ src/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './LandingPage.scss';
+
+function LandingPage() {
+  return (
+    <div id="home-page" className="page">
+      <div className="page-content">
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
